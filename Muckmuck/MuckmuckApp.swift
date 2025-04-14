2e2c53d48e2e1c71dd3453683cc8e92abee54b24
Rama <gudrms2538@gmail.com>
2025-04-14 23:22:17 +0900

    Initial Commit

---
+++ Muckmuck/MuckmuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuckmuckApp.swift
+//  Muckmuck
+//
+//  Created by Rama on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MuckmuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
