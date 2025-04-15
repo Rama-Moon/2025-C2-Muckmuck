df24ce54a67b42f5caed6f040f33113a7fa62bf4
Rama <gudrms2538@gmail.com>
2025-04-15 16:08:23 +0900

    feat: EventModel 구현, 더미데이터 구현

---
+++ Muckmuck/Extensions/UIScreen+.swift
@@ -0,0 +1,8 @@
+//
+//  UIScreen+.swift
+//  Muckmuck
+//
+//  Created by Rama on 4/15/25.
+//
+
+import Foundation
---
+++ Muckmuck/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  Muckmuck
+//
+//  Created by Rama on 4/14/25.
+//
+
+import Foundation
