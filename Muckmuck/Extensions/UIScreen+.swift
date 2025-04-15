5f3a40a97fb2f26b52a45ab01281a19469e5d122
Rama <gudrms2538@gmail.com>
2025-04-15 16:08:44 +0900

    Revert "feat: EventModel 구현, 더미데이터 구현"
    This reverts commit df24ce54a67b42f5caed6f040f33113a7fa62bf4.

--- Muckmuck/Extensions/UIScreen+.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIScreen+.swift
-//  Muckmuck
-//
-//  Created by Rama on 4/15/25.
-//
-
-import Foundation
--- Muckmuck/HomeView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HomeView.swift
-//  Muckmuck
-//
-//  Created by Rama on 4/14/25.
-//
-
-import Foundation
